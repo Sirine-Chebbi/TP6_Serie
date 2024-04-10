20b406fadd3594ef357d41d431eb8d283ab06e52
Sirine-Chebbi <152716295+Sirine-Chebbi@users.noreply.github.com>
2024-04-10 23:21:03 +0100

    Update IGenreDao.java

--- src/dao/IGenreDao.java
+++ src/dao/IGenreDao.java
@@ -6,8 +6,8 @@ import metier.entities.Genre;
 public interface IGenreDao {
 	
 	public Genre save(Genre g);
-	public Genre updateGenre(Genre cat);
-	public void deleteGenre(int id);
+	public Genre updateGenre(Genre g);
+	public void deleteGenre(long id);
 	public List<Genre> getAllGenres();
 	public Genre getGenre(long id);
 
